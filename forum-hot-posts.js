99e7ad9a7be51b9530b9e4c959af5ad47b8f3b57
Che-Wei Lin <linton.tw@gmail.com>
2015-07-31 00:20:52 +0800

    Change icon

--- forum-hot-posts.js
+++ forum-hot-posts.js
@@ -1,5 +1,7 @@
 var request = require('request');
 
+var GENDER_SYMBOL = {F: '<i class="woman icon"></i>', M: '<i class="man icon"></i>'};
+
 module.exports = {
 
   forumRequest: function(ForumName) {
@@ -12,7 +14,7 @@ module.exports = {
 
         for (var i = 0, postsCount = postsJson.length; i < postsCount; i++) {
 
-          var postThread = '<tr><td>' + postsJson[i].likeCount + '</td><td>' + postsJson[i].comment + '</td><td>' + postsJson[i].version[0].title + '</td><td>' + postsJson[i].member.gender + ' ' +  postsJson[i].member.school  + '</td></tr>';
+          var postThread = '<tr><td>' + postsJson[i].likeCount + '</td><td>' + postsJson[i].comment + '</td><td>' + postsJson[i].version[0].title + '</td><td>' + GENDER_SYMBOL[postsJson[i].member.gender] + ' ' +  postsJson[i].member.school  + '</td></tr>';
 
           $(domName).append(postThread);
         }
